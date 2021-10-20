2400d108f9e93cd81c68690ae69c45944f9d871b
PR1MEC0DE <92052172+PR1MEC0DE@users.noreply.github.com>
2021-10-20 13:45:08 +0300

    Update exercitiu.py

--- exercitiu.py
+++ exercitiu.py
@@ -2,5 +2,7 @@ print("Introdu lungimea cercului")
 L = float(input())
 r = (L/(2*3.1415))
 print("Raza cercului este",r, "cm")
+d = r*r
+print("Diametrul cercului este", d, "cm")
 a = r**2 * 3.1415
 print("Aria cercului este",a ,"cm²")
