4a82c5dc8aff24155268668933f8dc4c32490237
PR1MEC0DE <92052172+PR1MEC0DE@users.noreply.github.com>
2021-11-03 13:39:31 +0200

    Add files via upload

---
+++ phithon123.py
@@ -0,0 +1,10 @@
+print("Introdu cele doua numere, apoi rezultatul inmultirii")
+a= int(input())
+b= int(input())
+c= int(input())
+d = a*b
+if d==c:
+    print("Bravo, stii tabla inmultirii")
+else:
+    print("Esti cascat, nu stii tabla inmultirii")
+    
